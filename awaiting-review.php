198ccb79069fcaa48056dba4b41c1e428efda79c
Earthquake <earthquake.design88@gmail.com>
2023-02-10 15:35:06 +0700

    Rollback css code

--- awaiting-review.php
+++ awaiting-review.php
@@ -93,7 +93,7 @@ if (!($_SESSION['authenticated'])) {
                     <div class="row mb-2">
                         <div class="col-8">
                             <h3 class="card-title text-primary">
-                                <?= $lang['awaiting_review']['title_header'] ?> <span id="showCount" class="text-warning show-count"></span>
+                                <?= $lang['awaiting_review']['title_header'] ?> <span id="showCount" class="text-warning" style="font-size: 16px;"></span>
                             </h3>
                         </div>
                         <div class="col-4">
