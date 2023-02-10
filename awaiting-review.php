e1871490d6282ffca83dbb4eb9f9891a179efdc3
Earthquake <earthquake.design88@gmail.com>
2023-02-10 15:21:17 +0700

    Modify title header

--- awaiting-review.php
+++ awaiting-review.php
@@ -93,7 +93,7 @@ if (!($_SESSION['authenticated'])) {
                     <div class="row mb-2">
                         <div class="col-8">
                             <h3 class="card-title text-primary">
-                                <?= $lang['awaiting_review']['title_header'] ?> <span id="showCount" class="text-warning"></span>
+                                <?= $lang['awaiting_review']['title_header'] ?> <span id="showCount" class="text-warning showCount"></span>
                             </h3>
                         </div>
                         <div class="col-4">
